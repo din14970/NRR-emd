f93bc61d2ce021ec82825247802aa355dbb54ab2
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-04-10 15:16:23 +0200

    made explore_file compliant with TEMMETA changes

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -3,38 +3,39 @@ import argparse
 import logging
 import sys
 import os
-import numpy as np
 from pathlib import Path
 
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def main():
     from TEMMETA.basictools import data_io as bo
     parser = argparse.ArgumentParser()
     parser.add_argument("input",
-                        help = "path to EMD file")
-    args = parser.parse_args() #read the arguments, store in args
-    
-    #get input path and extension
+                        help="path to EMD file")
+    args = parser.parse_args()
+    # read the arguments, store in args
+
+    # get input path and extension
     inpath = args.input
     _, ext = os.path.splitext(inpath)
-    if not os.path.isfile(inpath) and ext=="emd":
+    if not os.path.isfile(inpath) and ext == "emd":
         logging.error("{} is not a valid emd file path".format(inpath))
         return
-    
-    #read in the file
+
+    # read in the file
     try:
-        f = bo.read_emd(inpath)
+        f = bo.EMDFile(inpath)
         logging.debug("Opened file {}".format(inpath))
-    except:
+    except OSError:
         logging.error("Did not manage to open file {}".format(inpath))
         return
-        
-    bo.print_simple_structure(f)
+
+    f.print_simple_structure()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     ownpath = os.path.abspath(__file__)
     folder, file = os.path.split(ownpath)
     folder = Path(folder)
