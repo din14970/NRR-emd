646cb74b6c3d8ea87312dd2509a0a27121461fde
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-06-29 12:51:04 +0200

    fixed issues with processing.py, still some irregularities

--- jnrr/processing.py
+++ jnrr/processing.py
@@ -6,9 +6,11 @@ from scipy import ndimage
 from pathlib import Path
 from .io_tools import read_config_file, loadFromQ2bz, _getNameCounterFrames
 import numpy as np
-from scipy.sparse import save_npz, csr_matrix
 import hyperspy.api as hs
+from scipy.sparse import csr_matrix
 from temmeta import data_io as dio
+
+logger = logging.Logger(name="Processing", level=logging.INFO)
 
 
 def write_as_image(img, path):
@@ -20,8 +22,16 @@ def calculate_non_rigid_registration(config_file):
     cmd = [str("matchSeries"), f"{config_file}", ">", "output.log"]
     process1 = subprocess.Popen(cmd, stderr=subprocess.PIPE)
     process1.wait()
-    logging.info("Finished non-rigid registration")
+    logger.info("Finished non-rigid registration")
     return read_config_file(config_file)["saveDirectory"]
+
+
+def apply_deformations_spectra():
+    pass
+
+
+def apply_deformations_images():
+    pass
 
 
 def apply_deformations(result_folder, image_folder=None,
@@ -72,7 +82,6 @@ def apply_deformations(result_folder, image_folder=None,
     defImagesFolder = parfolder+f"/deformedImages_{numbering}/"
     if not os.path.isdir(defImagesFolder):
         os.makedirs(defImagesFolder)
-    images.metadata.to_file(defImagesFolder+"/metadata.json")
     spec_list = []
     if spectra_folder is not None:
         specstr = dio.import_files_to_spectrumstream(spectra_folder)
@@ -80,16 +89,18 @@ def apply_deformations(result_folder, image_folder=None,
         defSpectraFolder = parfolder+f"/deformedSpectra_{numbering}/"
         if not os.path.isdir(defSpectraFolder):
             os.makedirs(defSpectraFolder)
-        images.metadata.to_file(defSpectraFolder+"/metadata.json")
     # loop over files
+    im_frm_list = []
+    spec_frm_list = []
     firstframe = True
     for i in range(frames):
         if i in skipframes:
             continue
         c = str(i).zfill(counter)
-        imname = str(Path(f"{parfolder}/{imfolder}/image_{c}.{imgext}"))
-        image = images.data[i]
-        logging.info(f"Processing frame {i}: {imname}")
+        imname = str(Path(
+            f"{parfolder}/{imfolder}/{dataBaseName}_{c}.{imgext}"))
+        image = images.get_frame(i)
+        logger.info(f"Processing frame {i}: {imname}")
         if firstframe:
             defX = loadFromQ2bz(str(Path(f"{result_folder}/stage{stage}/{i}/"
                                 f"deformation_{bznumber}_0.dat.bz2")))
@@ -101,22 +112,23 @@ def apply_deformations(result_folder, image_folder=None,
                                 f"deformation_{bznumber}_0.dat.bz2")))
             defY = loadFromQ2bz(str(Path(f"{result_folder}/stage{stage}/{i}-r/"
                                 f"deformation_{bznumber}_1.dat.bz2")))
-        h, w = image.shape
+        w = image.width
+        h = image.height
         coords = \
             np.mgrid[0:h, 0:w] + np.multiply([defY, defX], (np.max([h, w])-1))
-        deformedImage = ndimage.map_coordinates(image, coords, order=0,
-                                                mode='constant',
-                                                cval=image.mean())
-        defimpath = str(Path(f"{defImagesFolder}/{dataBaseName}_{c}.{imgext}"))
-        write_as_image(deformedImage, defimpath)
-        logging.info(f"Wrote out the deformed image to {defimpath}")
+        deformedData = ndimage.map_coordinates(image.data, coords, order=0,
+                                               mode='constant',
+                                               cval=image.data.mean())
+        defImage = dio.create_new_image(deformedData, image.pixelsize,
+                                        image.pixelunit, parent=image,
+                                        process=("Applied non rigid "
+                                                 "registration"))
+        im_frm_list.append(defImage)
         if spec_list:
-            logging.info("Reading spectra ... ")
+            logger.info("Correcting corresponding spectrum frame")
             spectra = spec_list[i]
-            logging.info("done")
             spectradef = dio.SpectrumStream._reshape_sparse_matrix(
                                 spectra, specstr.dimensions)
-            logging.info("Applying the deformations")
             image_stack = hs.signals.Signal2D(spectradef)
             image_stack.axes_manager[1].name = "x"
             image_stack.axes_manager[2].name = "y"
@@ -126,10 +138,9 @@ def apply_deformations(result_folder, image_folder=None,
                         inplace=False, parallel=True)
             result.unfold()
             defspec_sp = csr_matrix(result.data.T)  # sparse matrix rep
-            save_npz(str(Path(
-                f"{defSpectraFolder}/{dataBaseName}_{c}.npz")), defspec_sp)
-            logging.info("Wrote out the deformed spectrum")
+            spec_frm_list.append(defspec_sp)
     # also do the post processing, with temmeta it's a minor thing
+    logger.info("Calculating average image (undeformed)")
     resultFolder = str(Path(parfolder+f"/results_{numbering}/"))
     if not os.path.isdir(resultFolder):
         os.makedirs(resultFolder)
@@ -139,31 +150,33 @@ def apply_deformations(result_folder, image_folder=None,
     write_as_image(averageUndeformed.data,
                    str(Path(resultFolder+f"/imageUndeformed.{imgext}")))
     # average image from deformed
-    averageDeformed = dio.import_files_to_stack(defImagesFolder)
-    averageDeformed._create_child_stack(
-        True, averageDeformed.data, averageDeformed.pixelsize,
-        averageDeformed.pixelunit, averageDeformed,
-        ("Performed non-rigid registration alignment with "
-         f"config file {config_file}")
-    )
-    averageDeformed = averageDeformed.average()
+    logger.info("Calculating average image (deformed)")
+    defstack = dio.images_to_stack(im_frm_list)
+    averageDeformed = defstack.average()
     averageDeformed.to_hspy(str(Path(resultFolder+"/imageDeformed.hspy")))
     write_as_image(averageDeformed.data,
                    str(Path(resultFolder+f"/imageDeformed.{imgext}")))
+    # also write out frames to individual files
+    defstack.export_frames(defImagesFolder, name=dataBaseName, counter=counter)
     if spectra_folder is not None:
         # averaged spectrum
+        logger.info("Calculating average spectrum (undeformed)")
         spectrumUndeformed = specstr.spectrum_map
         spectrumUndeformed.to_hspy(str(Path(
             resultFolder+"/spectrumUndeformed.hspy")))
         # averaged spectrum deformed
-        spectrumDeformed = dio.import_files_to_spectrumstream(defSpectraFolder)
-        spectrumDeformed = spectrumDeformed.spectrum_map
-        spectrumDeformed = spectrumDeformed._create_child_map(
-            True, spectrumDeformed.data, spectrumDeformed.pixelsize,
-            spectrumDeformed.dispersion, spectrumDeformed.spectrum_offset,
-            ("Performed non-rigid registration alignment with "
-                f"config file {config_file}")
-        )
+        logger.info("Calculating average spectrum (deformed)")
+        specstr_data = dio.SpectrumStream._stack_frames(spec_frm_list)
+        specstr_def = dio.SpectrumStream(specstr_data,
+                                         specstr.metadata)
+        # edit the number of frames
+        specstr_def.metadata.data_axes["frame"]["bins"] = len(spec_frm_list)
+        specstr_def.export_streamframes(defSpectraFolder,
+                                        pre=dataBaseName,
+                                        counter=counter)
+        spectrumDeformed = specstr_def.spectrum_map
+        spectrumDeformed.to_hspy(str(Path(
+                resultFolder+"/spectrumDeformed.hspy")))
         return (averageUndeformed, averageDeformed,
                 spectrumUndeformed, spectrumDeformed)
     else:
